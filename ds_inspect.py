cf829e38a0b569b5a4d6f1b95cd262c3ebb3bfdd
sadzarik <vadiklooh@gmail>
2025-06-08 17:41:48 +0200

    Видалив старі дані

--- ds_inspect.py
+++
@@ -1,9 +0,0 @@
-# ds_inspect.py
-import itertools
-
-with open("datasets/datasets_for_scrapping/annotation_dataset.csv", encoding="utf-8-sig") as f:
-    for i, line in enumerate(f, start=1):
-        if i == 2065:
-            print("=== Рядок 2065 ===")
-            print(line)
-            break
