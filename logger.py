cb40ba6c4322142bc3c791b125bf2a797cbe1fe1
sadzarik <vadiklooh@gmail>
2025-06-11 00:03:38 +0200

    new logger

--- logger.py
+++ logger.py
@@ -14,7 +14,8 @@ logging.basicConfig(
     filename=log_file,
     level=logging.INFO,
     format="%(asctime)s [%(levelname)s] %(message)s",
-    datefmt="%Y-%m-%d %H:%M:%S"
+    datefmt="%Y-%m-%d %H:%M:%S",
+    encoding='utf-8'
 )
 
 def log_request(text, predicted_class, probabilities):
